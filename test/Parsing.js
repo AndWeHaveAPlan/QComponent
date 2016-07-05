fc1f35cebe96862e57aa37373555562392fa906d
likeawildfire <ravenor@ravenor.ru>
2016-07-05 19:41:38 +0300

    moar parsing tests

--- test/Parsing.js
+++ test/Parsing.js
@@ -12,8 +12,8 @@ describe("QScript parser", function () {
     it("should build tree", function () {
 
         var resultTree = Parser.parse('div: 123\n' +
-            '  Button: a/n\r\n' +
-            '  Button: b/n\n'
+            '  Button: a\r\n' +
+            '  Button: b\n'
         );
 
 
@@ -59,9 +59,9 @@ describe("QScript parser", function () {
 
         var resultTree = Parser.parse(
             'div: /*blah  */ 123\n' +
-            '  Button: a/n\n' +
+            '  Button: a\n' +
             ' //blah bla\n' +
-            '  Button: b/n\n'
+            '  Button: b\n'
         );
 
         var mustHaveTree = {
@@ -94,6 +94,103 @@ describe("QScript parser", function () {
                     children: []
                 }
             ]
+        };
+
+        mustHaveTree.children[0].parent = mustHaveTree;
+        mustHaveTree.children[1].parent = mustHaveTree;
+
+        assert.deepEqual(resultTree, mustHaveTree);
+    });
+
+    it("should ignore multiline comments", function () {
+
+        var resultTree = Parser.parse(
+            'div: /*blah  */ 123\n' +
+            ' /* wefdfg adfrsg dfg adegr \r\n' +
+            '*/Button: a/r\n' +
+            ' //blah bla\n' +
+            '  Button: b\r\n'
+        );
+
+        var mustHaveTree = {
+            col: 1,
+            row: 1,
+            pureLine: 'div:  123\r\n',
+            type: 'Div',
+            ravLine: 'div: /*blah  */ 123\r\n',
+            rawChildren: ' /* wefdfg adfrsg dfg adegr \r\n*/Button: a\r\n //blah bla\n  Button: b\r\n',
+            pureChildren: '  Button: a\r\n \r\n  Button: b\r\n',
+            children: [
+                {
+                    col: 3,
+                    row: 3,
+                    pureLine: 'Button: a\r\n',
+                    type: 'Button',
+                    ravLine: 'Button: a\r\n',
+                    rawChildren: '',
+                    pureChildren: '',
+                    children: []
+                },
+                {
+                    col: 3,
+                    row: 5,
+                    pureLine: 'Button: b\r\n',
+                    type: 'Button',
+                    ravLine: 'Button: b\r\n',
+                    rawChildren: '',
+                    pureChildren: '',
+                    children: []
+                }
+            ]
+        };
+
+        mustHaveTree.children[0].parent = mustHaveTree;
+        mustHaveTree.children[1].parent = mustHaveTree;
+
+        assert.deepEqual(resultTree, mustHaveTree);
+    });
+
+    it("deep nesting", function () {
+
+        var resultTree = Parser.parse(
+            'div: 123\n' +
+            '  div: 321\n' +
+            '    div: 1\n'
+        );
+
+        var mustHaveTree = {
+            col: 1,
+            row: 1,
+            pureLine: 'div: 123\r\n',
+            type: 'Div',
+            ravLine: 'div: 123\r\n',
+            rawChildren: '  div: 321\r\n    div: 1\r\n      Button: a\r\n      Button: b\r\n',
+            pureChildren: '  div: 321\r\n    div: 1\r\n      Button: a\r\n      Button: b\r\n',
+            children: []
+        };
+
+        mustHaveTree.children[0] = {
+            parent: mustHaveTree,
+            col: 3,
+            row: 2,
+            pureLine: '  div: 321\r\n',
+            type: 'Div',
+            ravLine: '  div: 321\r\n',
+            rawChildren: '    div: 1\r\n      Button: a\r\n      Button: b\r\n',
+            pureChildren: '    div: 1\r\n      Button: a\r\n      Button: b\r\n',
+            children: []
+        };
+
+        mustHaveTree.children[0].children[0] = {
+            parent: mustHaveTree.children[0],
+            col: 3,
+            row: 2,
+            pureLine: '    div: 1\r\n',
+            type: 'Div',
+            ravLine: '    div: 1\r\n',
+            rawChildren: '      Button: a\r\n      Button: b\r\n',
+            pureChildren: '      Button: a\r\n      Button: b\r\n',
+            children: []
         };
 
         mustHaveTree.children[0].parent = mustHaveTree;
