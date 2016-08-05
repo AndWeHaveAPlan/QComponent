820a40e639e397cc9b333eba91a09e366bffe8d8
likeawildfire <ravenor@ravenor.ru>
2016-08-05 19:11:27 +0300

    ItemTemplate value
    Former-commit-id: 7840b8ecb3596505a36315bd5da20d2fb9367004

--- Base/Components/UI/ItemTemplate.js
+++ Base/Components/UI/ItemTemplate.js
@@ -2,13 +2,26 @@
  * Created by ravenor on 13.07.16.
  */
 
+var Property = require('../../Property');
 var UIComponent = require('./../UIComponent');
+var Primitives = require('./Primitives');
 
 /**
  *
  */
 module.exports = UIComponent.extend('ItemTemplate', {
-
+    _prop: {
+        value: new Property('String', {description: 'text content'}, {
+            set: function (name, val) {
+                if (!this.textNode) {
+                    this.textNode = new Primitives['textNode'];
+                    this._children.unshift(this.textNode);
+                }
+                this.textNode.set('value', val);
+            },
+            get: Property.defaultGetter
+        })
+    }
 }, function (cfg) {
     UIComponent.call(this, cfg);
 });
