43c006e79a2a41979c8c183fd7a6f8b50471b437
likeawildfire <ravenor@ravenor.ru>
2016-08-16 21:42:27 +0300

    masked inpout

---
+++ Base/Components/UI/CardForm.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ravenor on 13.07.16.
+ */
+
+var Primitive = require('./Primitives');
+var UIComponent = require('../UIComponent');
+var Property = require('../../Property');
+
+
+module.exports = UIComponent.extend('Checkbox', {
+    createEl: function () {
+        var self = this;
+        this.el = UIComponent.document.createElement('input');
+        this.el.setAttribute('type', 'checkbox');
+        this.el.addEventListener('click', function (event) {
+            self.set('checked', this.checked);
+        });
+    },
+    _prop: {
+        checked: Property.generate.attributeProperty('checked'),
+        value: new Property('String', {},{overrideKey: 'checked'})
+    },
+    simlink: {
+
+    }
+});
