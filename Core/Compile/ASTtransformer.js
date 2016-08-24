4c92fc9a83ea85734fc2567f062bca2cb784d64e
Zibx <zibx.mail@gmail.com>
2016-08-25 00:31:57 +0300

    do not merge with this. code is broken. pipe ast transformation continue
    Former-commit-id: 71e06d4f1b0e6f037705ba914ab5bfa6aa59b929

--- Core/Compile/ASTtransformer.js
+++ Core/Compile/ASTtransformer.js
@@ -11,7 +11,7 @@ module.exports = (function(){
 
     var rules = {
         'Program': '*body',
-        'CallExpression,NewExpression': ['callee', '*arguments'],
+        'NewExpression': ['callee', '*arguments'],
         'ExpressionStatement': 'expression',
         'ArrayExpression': '*elements',
         'ConditionalExpression,IfStatement': ['test', 'consequent', 'alternate'],
@@ -43,7 +43,7 @@ module.exports = (function(){
 
 */
 
-    var doTransform = function(node){
+    var doTransform = function(node, options){
         if(!node) return;
 
         var type = node.type,
@@ -52,7 +52,7 @@ module.exports = (function(){
         if(!extractor){
             throw new Error('No extractor for type `'+ type +'`');
         }
-        return extractor.call(this, node);
+        return extractor.call(this, node, options);
     };
     var generateNumber = function(value){
         return {
@@ -73,28 +73,32 @@ module.exports = (function(){
             }
         };
     };
-
+    var mapWrapper = function(scope, a, b, c){
+        return function(item){
+            doTransform.call(scope, item, a, b, c);
+        };
+    };
     var extractors = {
-        'UpdateExpression': function(node){
+        'UpdateExpression': function(node, options){
             if(node.prefix){
-                return doTransform.call(this, node.argument);
+                return doTransform.call(this, node.argument, options);
             }else{
                 return doTransform.call( this, generateAssignment(
                     node.argument,
                     node.operator.charAt(0),
                     generateNumber(1)
-                ));
+                ), options);
             }
 
         },
-        'AssignmentExpression': function(node){
+        'AssignmentExpression': function(node, options){
 
             node = Object.create(node);
 
 
             /** if variable is declared - do nothing */
             if(!('_id' in node.left) || !(node.left._id in this)) {
-                node.right = doTransform.call(this,node.right);
+                node.right = doTransform.call(this,node.right, options);
                 return node;
             }
 
@@ -109,9 +113,16 @@ module.exports = (function(){
             //stack.push(pointer.property);
 
             if(node.operator !== '='){
-                return doTransform.call(this, generateAssignment(node.left, node.operator.substr(0, node.operator.length - 1), node.right));
+                return doTransform.call(
+                    this,
+                    generateAssignment(
+                        node.left, node.operator.substr(0, node.operator.length - 1),
+                        node.right
+                    ),
+                    options
+                );
             }
-            node.right = doTransform.call(this,node.right);
+            node.right = doTransform.call(this, node.right, options);
             var out = {
 
                     "type": "CallExpression",
@@ -131,7 +142,7 @@ module.exports = (function(){
                                 (stack.length ? stack.reverse().map(function(item){
                                     //console.log(JSON.stringify(item,null,2));
                                     if(item.computed){
-                                        return doTransform.call(_self, item);
+                                        return doTransform.call(_self, item, options);
                                     }else{
                                         return {
                                             "type": "Literal",
@@ -155,21 +166,25 @@ module.exports = (function(){
             //assert.deepEqual(node, out);
             return out;
         },
-        'BlockStatement': function(node){
-            return node;
+        'BlockStatement': function(node, options){
+            return node; // TODO unshit
         },
-        'MemberExpression': function(node){
+        'MemberExpression': function(node, options){
             var _self = this;
 
             if('_id' in node && node._id in this){
                 //console.log(JSON.stringify(node,null,2));
                 var ending = [], pointer = node, stack = [];
-                console.log(pointer, pointer.object)
+                //console.log(pointer, pointer.object)
                 while(pointer.object.type !== 'Identifier' && pointer.object.type !== 'ThisExpression'){
                     stack.push(pointer.property);
                     pointer = pointer.object;
                 }
                 stack.push(pointer.property);
+
+                if(options.variableTransformer){
+                    return options.variableTransformer(node);
+                }
 
                 return {
                         "type": "CallExpression",
@@ -189,7 +204,7 @@ module.exports = (function(){
                                     (stack.length ? stack.reverse().map(function(item){
                                         //console.log(JSON.stringify(item,null,2));
                                         if(item.computed){
-                                            return doTransform.call(_self, item);
+                                            return doTransform.call(_self, item, options);
                                         }else{
                                             return {
                                                 "type": "Literal",
@@ -209,20 +224,37 @@ module.exports = (function(){
                 };
             }else {
                 node = Object.create(node);
-                node.object = doTransform.call(this,node.object);
-                node.property = doTransform.call(this,node.property);
+
+                node.object = doTransform.call(this, node.object, options);
+                node.property = doTransform.call(this, node.property, options);
                 return node;
             }
         },
         'FunctionExpression': function(node){
-            return node;
+            return node; // TODO unshit
+        },
+        'CallExpression': function(node, options){
+            return {
+                "type": "CallExpression",
+                "callee": {
+                    "type": "MemberExpression",
+                    "computed": node.callee.computed,
+                    "object": doTransform.call(this, node.callee.object, options),
+                    "property": node.callee.computed ? // IF property is dynamic - try to get deeper
+                        doTransform.call(this,node.callee.property, options) : node.callee.property
+                },
+                "arguments": node.arguments.map(mapWrapper(this,options))
+            };
+
         },
         'FunctionDeclaration': function(node){
-            return node;
+            return node; // TODO unshit
         },
-        'Identifier': function(node){
+        'Identifier': function(node, options){
             if('_id' in node && node._id in this){
-                console.log(node)
+                if(options.variableTransformer){
+                    return options.variableTransformer(node);
+                }
                 return {
                         "type": "CallExpression",
                         "callee": {
@@ -268,16 +300,19 @@ module.exports = (function(){
         for(var i in rules){
             if(rules.hasOwnProperty(i)){
                 var val = rules[i];
-                var fn = new Function('doTransform', 'return function(node){node = Object.create(node); '+
+                var fn = new Function('doTransform', 'return function(node, options){' +
+                    'node = Object.create(node); '+
                     (val instanceof Array ? val : [val]).map(function(statement){
                         var each;
                         if(statement !== null) {
                             (each = statement.charAt(0) === '*') &&
                             (statement = statement.substr(1));
                             if (each) {
-                                return 'node[\'' + statement + '\'] = node[\'' + statement + '\'].map(doTransform, this);';
+                                return 'node[\'' + statement + '\'] = node[\'' + statement + '\'].map(function(item){' +
+                                        'return doTransform.call(this, item, options);' +
+                                    '}, this);';
                             } else {
-                                return 'node[\'' + statement + '\'] = doTransform.call(this,node[\'' + statement + '\']);';
+                                return 'node[\'' + statement + '\'] = doTransform.call(this, node[\'' + statement + '\'], options);';
                             }
                         }
                     }).join('\n')+';return node;}');
@@ -297,15 +332,26 @@ module.exports = (function(){
                     });
                 }
             }
+            options = options || {};
 
+            var before = doTransform.call(list,esprimaTree, options);
 
-            var before = doTransform.call(list,esprimaTree);
-
-            return escodegen.generate(before, {
-                format: options || {}
-            });
+            return escodegen.generate(before, options.escodegen);
         }
 
+    };
+
+    ASTtransformer.craft = {
+        Identifier: function(name){
+            return {
+                "type": "Identifier",
+                "name": name
+            }
+        },
+        js: function(tree, options){
+            options = options || {};
+            return escodegen.generate(tree, options.escodegen);
+        }
     };
     return ASTtransformer;
 })();
