cd356a96baa21bc857c1e13c6428c2e8fc32b484
Zibx <zibx.mail@gmail.com>
2016-09-06 00:17:45 +0300

    eventManager

--- Base/Common/UIEventManager.js
+++ Base/Common/UIEventManager.js
@@ -1,3 +1,14 @@
 /**
  * Created by zibx on 17.08.16.
  */
+module.exports = (function(){
+    'use strict';
+    var Keyboard = require('../Common/UI/Keyboard'),
+        QObject = require('../QObject');
+
+    var UIEventManager = function(){};
+    UIEventManager.prototype = {
+        
+    };
+    return UIEventManager;
+})();
