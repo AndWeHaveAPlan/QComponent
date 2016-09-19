bd06468ed3795cde3f58c7d8c408d9cd10f677c0
Misha <mishapepyaka@gmail.com>
2016-09-19 23:59:07 +0300

    Делаем потихоньку время для аудио, но тут все развалилось
    Former-commit-id: 70f70d08fedf5084c37e5aa7dfa8d43dda495291

--- Base/Components/Logical/Audio.js
+++ Base/Components/Logical/Audio.js
@@ -12,16 +12,24 @@ module.exports = LogicalComponent.extend('Audio', {
     pause: function () {
         this.audio.pause();
     },
-    _prop:{
+    _prop: {
         pause: new Property('Function'),
         play: new Property('Function'),
+        time: new Property('Number', {}, {
+            set: function (key, val) {
+                this.audio.currentTime = val;
+            },
+            get: function () {
+                return this.audio.currentTime;
+            }
+        }),
         playOnStart: new Property('Boolean', {description: 'Do we need play audio on start?'}, void 0, true),
-        value: new Property('String',{description : 'Url of audio source'},
+        value: new Property('String', {description: 'Url of audio source'},
             {
                 get: Property.defaultGetter,
                 set: function (name, value) {
                     this.audio.src = value;
-                    if( this.get('playOnStart') )
+                    if (this.get('playOnStart'))
                         this.play();
                 }
             })
