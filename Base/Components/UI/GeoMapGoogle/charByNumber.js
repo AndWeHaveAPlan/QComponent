5cb8117de474de008cb0d32f3c3538c84c4ebc49
likeawildfire <esnitko@outlook.com>
2016-09-23 12:37:55 +0300

    charByNumber impoved!!11

--- Base/Components/UI/GeoMapGoogle/charByNumber.js
+++ Base/Components/UI/GeoMapGoogle/charByNumber.js
@@ -2,9 +2,16 @@
  * Created by nikita on 30.08.16.
  */
 
-module.exports = function(num) {
-  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var ind = num % alphabet.length;
+module.exports = function (num) {
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var ind = num % alphabet.length;
+    var t = Math.floor(num / alphabet.length);
 
-  return alphabet[ind];
+    var ret = '';
+    while (t >= 0) {
+        ret += alphabet[ind];
+        t--;
+    }
+
+    return ret;
 }
