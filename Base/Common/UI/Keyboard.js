e0f84ba0db60ebe38daff443eab9f7e51d5be105
Zibx <zibx.mail@gmail.com>
2016-09-27 23:52:08 +0300

    default subscriber

--- Base/Common/UI/Keyboard.js
+++ Base/Common/UI/Keyboard.js
@@ -11,25 +11,34 @@ module.exports = (function () {
 
     var KB = function (layer) {
         this.layer = layer;
+        this.elseFns = {};
     };
+
     KB.prototype = {
         on: function(cfg){
             var i, keyCode = DOM.keyCode, map = {},
-                elseFns = {},
                 _self = this;
 
             for(i in cfg){
                 map[keyCode[i]] = cfg[i];
             }
             this.layer.keydown = function(e){
-                var what = map[e.which];
+                var what = map[e.which], result;
 
-                if(what)
-                    return what.call(_self.layer.owner, e);
+                if(what) {
+                    result = what.call(_self.layer.owner, e);
+                    if(result !== false)
+                        return result;
+                }
+                _self.elseFns.keydown && _self.elseFns.keydown(e);
 
                 e.preventDefault();
             };
-
+        },
+        defaultSubscriber: function(element){
+            this.elseFns.keydown = function(e){
+                element.value+= e.key;
+            };
         }
     };
     return KB;
