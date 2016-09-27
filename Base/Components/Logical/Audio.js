26429c32c2c35a5c0c02e16f4801c93039a4ca18
Misha <mishapepyaka@gmail.com>
2016-09-27 16:31:16 +0300

    Added AudioDemo.qs and stuff

--- Base/Components/Logical/Audio.js
+++ Base/Components/Logical/Audio.js
@@ -7,34 +7,84 @@ var Property = require('../../Property');
 
 module.exports = LogicalComponent.extend('Audio', {
     play: function () {
-        this.audio.play();
+        this.el.play();
+    },
+    stop: function () {
+        this.el.pause();
     },
     pause: function () {
-        this.audio.pause();
+        this.el.pause();
+    },
+    createEl: function () {
+
     },
     _prop: {
         pause: new Property('Function'),
         play: new Property('Function'),
-        time: new Property('Number', {}, {
-            set: function (key, val) {
-                this.audio.currentTime = val;
-            }, 
+        time: new Property('Number', { description: 'Current playback position' }, {
             get: function () {
-                return this.audio.currentTime;
+                return this.el.currentTime;
+            },
+            set: function (name, value, oldValue, e) {
+                if(!this.updating)
+                    this.el.currentTime = value;
             }
         }),
-        playOnStart: new Property('Boolean', {description: 'Do we need play audio on start?'}, void 0, true),
-        value: new Property('String', {description: 'Url of audio source'},
+        duration: new Property('Number', { }),
+        volume: new Property('Number', { description: 'Current volume' }, {
+            get: function () {
+                return this.el.volume;
+            },
+            set: function (name, value, oldValue, e) {
+                this.el.volume = value;
+            }
+        }),
+        controls: new Property('Boolean', { }, {
+            get: function () {
+                return this.el.controls;
+            },
+            set: function (name, value, oldValue, e) {
+                this.el.controls = value;
+            }
+        }),
+        muted: new Property('Boolean', { }, {
+            get: function () {
+                return this.el.muted;
+            },
+            set: function (name, value, oldValue, e) {
+                this.el.muted = value;
+            }
+        }),
+        autoplay: new Property('Boolean',
             {
-                get: Property.defaultGetter,
-                set: function (name, value) {
-                    this.audio.src = value;
-                    if (this.get('playOnStart'))
+                set: function(name, value) {
+                    if (value)
                         this.play();
                 }
-            })
+            }),
+        value: new Property('String', { description: 'URL of the audio' }, {
+            get: Property.defaultGetter,
+            set: function (name, value, oldValue, e) {
+                this.stop();
+                this.el.src = value;
+
+                if (this.get('autoplay')) {
+                    this.play();
+                }
+            }
+        })
     }
 }, function () {
     LogicalComponent.apply(this, arguments);
-    this.audio = new Audio();
+    this.el = this.audio = LogicalComponent.document.createElement('video');
+    var self = this;
+
+    this.el.addEventListener('timeupdate', function (event) {
+        self.updating = true;
+        self.set('time', self.el.currentTime);
+        self.updating = false;
+    });
+    this.el.addEventListener('durationchange', function (event) {
+        self.set('duration', self.el.duration);
+    });
 });
