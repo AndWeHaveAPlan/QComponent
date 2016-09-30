1f1ddfceabdce0d94529b7f8c2bb8c11243fd981
ravenor <ravenor@ravenor.ru>
2016-09-30 17:02:39 +0300

    tabControl headerTemplate fix

--- Base/Components/UIComponent.js
+++ Base/Components/UIComponent.js
@@ -253,7 +253,8 @@ module.exports = (
                     child.renderTo = insertInto;
                 }
 
-                child.updateLayout();
+                if(child instanceof UIComponent)
+                    child.updateLayout();
                 this.bubble('childAdded', { child: child });
 
             },
